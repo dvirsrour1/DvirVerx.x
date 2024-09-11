91e7581bcf4712a3fa1212a767640217c512aac0
Dvir Srour <dvirsrour1@gmail.com>
2024-09-11 11:30:30 +0300

    adding CORS 11/9/2024

--- src/main/java/applicationVertx/verticles/http/RouterClass.java
+++ src/main/java/applicationVertx/verticles/http/RouterClass.java
@@ -3,6 +3,7 @@ package applicationVertx.verticles.http;
 import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.ext.web.handler.CorsHandler;
 
 public class RouterClass extends HttpHandlers {
 
@@ -17,6 +18,16 @@ public class RouterClass extends HttpHandlers {
         router.delete("/DeleteTask").handler(this::deleteTask);
         router.get("/Tasks").handler(this::showTask);
 
+
+        router.route().handler(CorsHandler.create("*")  // מאפשר לכל מקור לגשת
+                .allowedMethod(io.vertx.core.http.HttpMethod.GET)
+                .allowedMethod(io.vertx.core.http.HttpMethod.POST)
+                .allowedMethod(io.vertx.core.http.HttpMethod.OPTIONS)
+                .allowedMethod(io.vertx.core.http.HttpMethod.DELETE) // אם נדרש
+                .allowedHeader("Content-Type") // כותרת שמתאימה לנתונים שנשלחים
+                .allowedHeader("Authorization") // אם אתה שולח אישורים
+                .allowCredentials(true)); // אם יש דרישה ל-Credentials כמו עוגיות
+ //allow everyone
         return router;
     }
 }
